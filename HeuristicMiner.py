35a0f6543753a541951118da710cc42192e589d9
Jack748 <56539660+Jack748@users.noreply.github.com>
2020-10-01 12:36:51 +0200

    Complete commitments finder
    input files in csv format

--- HeuristicMiner.py
+++ HeuristicMiner.py
@@ -1,93 +1,112 @@
-"""
-
 import pandas as pd
 from pm4py.objects.log.util import dataframe_utils
 from pm4py.objects.conversion.log import converter as log_converter
 
-log_csv = pd.read_csv('15CASI-3EVAL_noquote.csv', sep=',')
+log_csv = pd.read_csv('totale.csv', sep=',')
 log_csv = dataframe_utils.convert_timestamp_columns_in_df(log_csv)
-parameters = {log_converter.Variants.TO_EVENT_LOG.value.Parameters.CASE_ID_KEY: 'NEWCASEID'}
 log_csv = log_csv.sort_values('TIMESTAMP')
-event_log = log_converter.apply(log_csv, parameters=parameters, variant=log_converter.Variants.TO_EVENT_LOG)
+log_csv.rename(columns={'ACTIVITY': 'concept:name'}, inplace=True)
+parameters = {log_converter.Variants.TO_EVENT_LOG.value.Parameters.CASE_ID_KEY: 'NEWCASEID'}
+log = log_converter.apply(log_csv, parameters=parameters, variant=log_converter.Variants.TO_EVENT_LOG)
 
-"""
-
-
-from pm4py.objects.log.importer.xes import factory as xes_importer
-import os
-log_path = os.path.join("15casi_corretto.xes")
-log = xes_importer.apply(log_path)
-
-
-from pm4py.algo.filtering.log.attributes import attributes_filter
-activities = attributes_filter.get_attribute_values(log, "concept:name")
 
 from pm4py.algo.discovery.heuristics import factory as heuristics_miner
 heu_net = heuristics_miner.apply_heu(log)
 
+#visualization of the heuristic net
+# from pm4py.visualization.heuristics_net import visualizer as hn_visualizer
+# gviz = hn_visualizer.apply(heu_net)
+# hn_visualizer.view(gviz)
 
+#obtaining resource of an activity
 def get_resource(activity):
     for trace in log:
         for event in trace:
             if event["concept:name"] == activity:
-                return event["RESOURCE"][:-1]
+                return event["RESOURCE"].strip()
 
-"""
-for node in heu_net.nodes:
-    print (node)
-"""
-
+#creating the list of the dependencies of the activities with the correspondent resources
 dep_list = []
-
 for activity in heu_net.dependency_matrix:
     for act in heu_net.dependency_matrix[activity].keys():
-        dependency = "{}({}) -> {}({}) = {}".format(activity, get_resource(activity), act, get_resource(act), heu_net.dependency_matrix[activity][act])
-        dep_list.append([activity, get_resource(activity), act, get_resource(act), heu_net.dependency_matrix[activity][act]])
+        dep_list.append([activity.strip(), get_resource(activity), act.strip(), get_resource(act), heu_net.dependency_matrix[activity][act]])
+        #dependency = (activity1, resource1, activity2, resource2)
 
+print("\n\n DEPENDENCIES:")
 for dep in dep_list:
     print(dep)
 
+#calculate the number of total final activities
+number_of_final_activities = 0
+for key in heu_net.end_activities[0].keys():
+    number_of_final_activities += heu_net.end_activities[0][key]
 
+#used to check if an activity is not in more than 5% of the finals ones
+def check_not_final(activity):
+    if activity in heu_net.end_activities[0].keys():
+        if (heu_net.end_activities[0][activity] / number_of_final_activities * 100) > 5:
+            print("percentage of",activity, "=",  heu_net.end_activities[0][activity] / number_of_final_activities * 100)
+            return False
+    return True
+
+#creation of all the basics commitments
+#note that debtor and creditor, for now, can be the same
 ccs = []
-
 for dep in dep_list:
-    #creazione di tutti i commit
-    #cancella dep[1] != dep[3] per avere commit della stessa persona
-    if  dep[4] >= 0.8:
-        ccs.append([dep[1], dep[3], dep[0], dep[2]])
-            
-print("\nCOMMITMENTS:\n")
+    if  dep[4] > 0.8 and check_not_final(dep[0]):
+        ccs.append([dep[3], dep[1], dep[0], dep[2]])
+        #cc(debtor, creditor, antecedent, consequent)
+
+print("\nCOMMITMENTS:")
 for cc in ccs:
     print(cc)
 
-#creazione dei commit con OR
-add_ccs = []
 
+#union of comitments with OR
+add_ccs = []
 for cc1 in ccs:
     for cc2 in ccs:
         if cc1 != cc2 and cc1[2] == cc2[2] and cc1[3] != cc2[3]:
+        #antecedent equal but consequent different
             add_ccs.append([cc1[0], cc1[1], cc1[2], "(" + cc1[3] + " V " + cc2[3] + ")"])
-            if cc1 in ccs:
-                ccs.remove(cc1)
-            if cc2 in ccs:
-                ccs.remove(cc2)
+            ccs.remove(cc1)
+            ccs.remove(cc2)
 
 for cc in add_ccs:
     ccs.append(cc)
 
-#concatenazione dei commit
-for i in range(3):
+print("\nCOMMITMENTS WITH OR:")
+for cc in ccs:
+    print(cc)
+
+
+#concatenation of commitments
+#the range is the maximum length of a concatenation
+for i in range(5):
     for cc1 in ccs:
         for cc2 in ccs:
-            if cc1[3][-5] == cc2[2][-5] and cc1[0] != cc2[1]:
-                cc2[0] = cc1[0]
-                cc2[2] = cc1[2] + " . " + cc1[3]
-                ccs.remove(cc1)
+            if cc1 != cc2 and cc1[3] == cc2[2] and cc1[1] != cc2[0]: #checking also that creditor of cc1 and debtor of cc2 are not the same
+                if cc2[0] == cc1[0]: #making sure that no activities of the debtor will be in the antecedent
+                    cc1[3] = cc1[3] + " . " + cc2[3] #putting those activities in the consequent instead
+                    ccs.remove(cc2)
+                else: #putting the activities in the antecedent
+                    cc2[1] = cc1[1]
+                    cc2[2] = cc1[2] + " . " + cc2[2]
+                    ccs.remove(cc1)
 
-#aggiunta del precedente all'antecedente
+print("\nCOMMITMENTS CONCATENATED:")
+for cc in ccs:
+    print(cc)
+
+#adding the antecedent to the consequent
 for cc in ccs:
     cc[3] = cc[2] + " . " + cc[3]
 
-print("\nFINAL COMMITMENTS:\n")
+#removing commitments where debtor is creditor too
+for cc in ccs:
+    if cc[0] == cc[1]:
+        ccs.remove(cc)
+
+print("\nFINAL COMMITMENTS:")
 for cc in ccs:
     print(cc)
